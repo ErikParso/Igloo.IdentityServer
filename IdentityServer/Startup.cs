b93c52f0025a8533c5a3f1041ea58147c5803219
Erik Parso <parsoerik@gmail.com>
2020-01-07 09:59:26 -0800

    Identity server MVC.

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,3 +1,5 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -5,24 +7,48 @@ using Microsoft.Extensions.Hosting;
 
 namespace IdentityServer
 {
-	public class Startup
-	{
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddIdentityServer()
-				.AddDeveloperSigningCredential()
-				.AddInMemoryApiResources(Config.GetApiResources())
-				.AddInMemoryClients(Config.GetClients());
-		}
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+			// uncomment, if you want to add an MVC-based UI
+			services.AddControllersWithViews();
+
+			var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.Ids)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients);
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+			// uncomment if you want to add MVC
+			app.UseStaticFiles();
+			app.UseRouting();
 
 			app.UseIdentityServer();
+
+			// uncomment, if you want to add MVC
+			app.UseAuthorization();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapDefaultControllerRoute();
+			});
 		}
-	}
+    }
 }
