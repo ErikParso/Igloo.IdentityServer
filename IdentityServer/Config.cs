d3b81ddf290d2a6a0f1d277ba9430cf03757683d
Erik Parso <parsoerik@gmail.com>
2020-05-03 19:52:36 +0200

    Test MVC client.

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,26 +2,50 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
 namespace IdentityServer
 {
-    public static class Config
-    {
-        public static IEnumerable<IdentityResource> Ids =>
-            new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
-            };
+	public static class Config
+	{
+		public static IEnumerable<IdentityResource> Ids =>
+			new List<IdentityResource>
+			{
+				new IdentityResources.OpenId(),
+				new IdentityResources.Profile(),
+			};
 
-        public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
-        
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
-    }
+		public static IEnumerable<ApiResource> Apis =>
+			new ApiResource[]
+			{ };
+
+		public static IEnumerable<Client> Clients =>
+			new List<Client>
+			{
+				// interactive ASP.NET Core MVC client
+				new Client
+				{
+					ClientId = "mvc",
+					ClientSecrets = { new Secret("secret".Sha256()) },
+
+					AllowedGrantTypes = GrantTypes.Code,
+					RequireConsent = false,
+					RequirePkce = true,
+
+					// where to redirect to after login
+					RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+					// where to redirect to after logout
+					PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+					AllowedScopes = new List<string>
+					{
+						IdentityServerConstants.StandardScopes.OpenId,
+						IdentityServerConstants.StandardScopes.Profile
+					}
+				}
+			};
+	}
 }
