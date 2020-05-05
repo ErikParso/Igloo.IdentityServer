f09a14870d943d738b308204c60aa83eabb1cf91
Erik Parso <parsoerik@gmail.com>
2020-05-05 18:57:51 +0200

    Logout + more identity resources.

---
+++ MVCClient/Controllers/LogoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCClient.Controllers
+{
+	public class LogoutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return SignOut("Cookies", "oidc");
+		}
+	}
+}
