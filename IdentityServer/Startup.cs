1b6361378eb58f1f04394d31e9900ec4b84d9eef
Erik Parso <parsoerik@gmail.com>
2020-05-12 17:53:03 +0200

    Using EF core for configuration and operational data.

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,10 +1,15 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Reflection;
 
 namespace IdentityServer
 {
@@ -22,11 +27,21 @@ namespace IdentityServer
 			// uncomment, if you want to add an MVC-based UI
 			services.AddControllersWithViews();
 
+			var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+			const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-3.0.0;trusted_connection=yes;";
+
 			var builder = services.AddIdentityServer()
-				.AddInMemoryIdentityResources(Config.Ids)
-				.AddInMemoryApiResources(Config.Apis)
-				.AddInMemoryClients(Config.Clients)
-				.AddTestUsers(TestUsers.Users);
+				.AddTestUsers(TestUsers.Users)
+				.AddConfigurationStore(options =>
+				{
+					options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+						sql => sql.MigrationsAssembly(migrationsAssembly));
+				})
+				.AddOperationalStore(options =>
+				{
+					options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+						sql => sql.MigrationsAssembly(migrationsAssembly));
+				});
 
 			services.AddAuthentication()
 				.AddGoogle("Google", options =>
@@ -42,6 +57,8 @@ namespace IdentityServer
 
 		public void Configure(IApplicationBuilder app)
 		{
+			InitializeDatabase(app);
+
 			if (Environment.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
@@ -59,6 +76,43 @@ namespace IdentityServer
 			{
 				endpoints.MapDefaultControllerRoute();
 			});
+		}
+
+		private void InitializeDatabase(IApplicationBuilder app)
+		{
+			using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+			{
+				serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+				var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+				context.Database.Migrate();
+				if (!context.Clients.Any())
+				{
+					foreach (var client in Config.Clients)
+					{
+						context.Clients.Add(client.ToEntity());
+					}
+					context.SaveChanges();
+				}
+
+				if (!context.IdentityResources.Any())
+				{
+					foreach (var resource in Config.Ids)
+					{
+						context.IdentityResources.Add(resource.ToEntity());
+					}
+					context.SaveChanges();
+				}
+
+				if (!context.ApiResources.Any())
+				{
+					foreach (var resource in Config.Apis)
+					{
+						context.ApiResources.Add(resource.ToEntity());
+					}
+					context.SaveChanges();
+				}
+			}
 		}
 	}
 }
