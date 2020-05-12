30c48190751f0cacd684caf0cc60d8740deb12a1
Erik Parso <parsoerik@gmail.com>
2020-05-12 17:11:26 +0200

    Test JS client tutorial.

--- TestApi/Startup.cs
+++ TestApi/Startup.cs
@@ -18,12 +18,24 @@ namespace TestApi1
 					options.RequireHttpsMetadata = false;
 					options.Audience = "api1";
 				});
+
+			services.AddCors(options =>
+			{
+				// this defines a CORS policy called "default"
+				options.AddPolicy("default", policy =>
+				{
+					policy.WithOrigins("http://localhost:5003")
+						.AllowAnyHeader()
+						.AllowAnyMethod();
+				});
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			app.UseRouting();
+			app.UseCors("default");
 			app.UseAuthentication();
 			app.UseAuthorization();
 			app.UseEndpoints(endpoints =>
